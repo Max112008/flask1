230f2c1ea70fc3f67a59c228ca638b440f40b8ec
Max112008 <elanskiym@bk.ru>
2025-02-20 19:38:56 +0300

    task4

--- main.py
+++ main.py
@@ -29,7 +29,38 @@ def promotion():
 @app.route('/image_mars')
 def image_mars():
     return f'''<title>Привет, Марс!</title><h1>Жди нас, Марс!</h1><img src="{flask.url_for('static', filename='images/MARS.png')}"> 
-               <p>вот она</p>'''
+           <div class="p-3 mb-2 bg-primary text-white">.Человечество вырастает из детства.</div>
+            <div class="p-3 mb-2 bg-secondary text-white">.Человечеству мала одна планета.</div>
+            <div class="p-3 mb-2 bg-success text-white">.Мы сделаем обитаемыми безжизненные пока планеты.</div>
+            <div class="p-3 mb-2 bg-danger text-white">.И начнем с Марса!</div>
+            <div class="p-3 mb-2 bg-warning text-dark">.Присоединяйся!</div>'''
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return f'''<title>Привет, Марс!</title><h1>Жди нас, Марс!</h1><
+    
+    img src="{flask.url_for('static', filename='images/MARS.png')}"> 
+           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+           <div class="p-3 mb-2 bg-primary text-white">.Человечество вырастает из детства.</div>
+            <div class="p-3 mb-2 bg-secondary text-white">.Человечеству мала одна планета.</div>
+            <div class="p-3 mb-2 bg-success text-white">.Мы сделаем обитаемыми безжизненные пока планеты.</div>
+            <div class="p-3 mb-2 bg-danger text-white">.И начнем с Марса!</div>
+            <div class="p-3 mb-2 bg-warning text-dark">.Присоединяйся!</div>'''
+
+@app.route('/sample_page')
+def return_sample_page():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{flask.url_for('static', filename='css/style.css')}" />
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Первая HTML-страница</h1>
+                  </body>
+                </html>"""
 
 
 if __name__ == '__main__':
